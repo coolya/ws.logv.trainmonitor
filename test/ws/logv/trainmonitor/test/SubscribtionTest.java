2e19bf24e8f62b7fa480b7419d4e1657c236062d
Kolja Dummann <k.dummann@gmail.com>
2012-10-25 21:25:20 +0200

    new assets

---
+++ test/ws/logv/trainmonitor/test/SubscribtionTest.java
@@ -0,0 +1,11 @@
+package ws.logv.trainmonitor.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kolja
+ * Date: 16.10.12
+ * Time: 17:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class SubscribtionTest {
+}
