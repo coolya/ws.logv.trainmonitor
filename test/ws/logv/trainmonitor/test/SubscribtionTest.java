27094e817206205629e61b848403dbc18a8cbaa1
Kolja Dummann <k.dummann@gmail.com>
2012-10-25 21:26:04 +0200

    stuff

--- test/ws/logv/trainmonitor/test/SubscribtionTest.java
+++ test/ws/logv/trainmonitor/test/SubscribtionTest.java
@@ -1,5 +1,7 @@
 package ws.logv.trainmonitor.test;
 
+import junit.framework.TestCase;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Kolja
@@ -7,5 +9,5 @@ package ws.logv.trainmonitor.test;
  * Time: 17:35
  * To change this template use File | Settings | File Templates.
  */
-public class SubscribtionTest {
+public class SubscribtionTest extends TestCase {
 }
