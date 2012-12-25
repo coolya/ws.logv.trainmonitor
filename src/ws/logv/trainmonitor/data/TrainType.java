0e272782f20c6517a3392e3e29dc6cb8de2f5fd8
Kolja Dummann <k.dummann@gmail.com>
2012-12-25 14:16:26 +0100

    #TM-64 Create Licence Fragment

---
+++ src/ws/logv/trainmonitor/data/TrainType.java
@@ -0,0 +1,12 @@
+package ws.logv.trainmonitor.data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kdummann
+ * Date: 07.12.12
+ * Time: 15:42
+ * To change this template use File | Settings | File Templates.
+ */
+public enum TrainType {
+    IC, ICE, EC, CNL, RJ, TGV, THA
+}
